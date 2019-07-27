c7a0ded3b64c7b10ae4f10c8cb594d40c44e8ea8
michirums <myc64sky@gmail.com>
2019-07-27 16:35:46 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,14 +8,13 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    respond_to do |format|
-      format.html
-      format.json 
-    end
     if @message.save
-      redirect_to group_messages_path(@group), notice: ' メッセージが送信されました'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+        format.json 
+      end
     else
-      @messsages = @group.messages.includes(:user)
+      @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
