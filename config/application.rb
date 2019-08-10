6d84914ad66334eacf4f7b5a7f93a65e93684dd8
michirums <myc64sky@gmail.com>
2019-08-10 18:29:02 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,8 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      end
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
-    end
   end
 end
